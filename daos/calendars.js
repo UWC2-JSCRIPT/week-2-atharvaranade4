45ec5e35d6b13cebb9e62cd7890c5d093b72f8d9
Atharva Ranade <atharvaranade4@gmail.com>
2023-04-25 21:08:54 -0700

    finish calender routes

--- daos/calendars.js
+++ daos/calendars.js
@@ -6,6 +6,15 @@ module.exports.create = async (name) => {
   return await Calendars.create({ name });
 };
 
+module.exports.getAll = async () => {
+  try {
+    const calendar = await Calendars.find().lean();
+    return calendar;
+  } catch (e) {
+    return null;
+  }
+};
+
 module.exports.getById = async (id) => {
   try {
     const calendar = await Calendars.findOne({ _id: id }).lean();
@@ -18,6 +27,15 @@ module.exports.getById = async (id) => {
 module.exports.updateById = async (id, newData) => {
   try {
     const calendar = await Calendars.findOneAndUpdate({ _id: id }, newData, { new: true }).lean();
+    return calendar;
+  } catch (e) {
+    return null;
+  }
+};
+
+module.exports.removeById = async (id) => {
+  try {
+    const calendar = await Calendars.findOneAndDelete({ _id: id }).lean();
     return calendar;
   } catch (e) {
     return null;
